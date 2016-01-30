90ffead79e6e61de453f77dcd24482792b00997c
Luis Fernando Mendoza <goliat8903@gmail.com>
2016-01-29 19:04:53 -0600

    Adds search directive and controller to show posts by slug

--- src/themes/bluepress/js/scripts.js
+++ src/themes/bluepress/js/scripts.js
@@ -1,5 +1,7 @@
-angular.module('app', ['ngRoute', 'ngSanitize'])
-.config( function( $routeProvider, $locationProvider ){
+var app = angular.module('app', ['ngRoute', 'ngSanitize']);
+
+// Configure the routes
+app.config(['$routeProvider', '$locationProvider', function( $routeProvider, $locationProvider ){
   $locationProvider.html5Mode(true);
 
   $routeProvider
@@ -7,20 +9,47 @@ angular.module('app', ['ngRoute', 'ngSanitize'])
     templateUrl: bpLocalized.partials + 'main.html',
     controller: 'Main'
   })
+
+  .when('/:slug', {
+    templateUrl: bpLocalized.partials + 'content.html',
+    controller: 'Content'
+  })
+
   .when('/:ID', {
     templateUrl: bpLocalized.partials + 'content.html',
     controller: 'Content'
   });
+}]);
 
-})
-.controller( 'Main', function( $scope, $http, $routeParams ) {
+
+// Main Controller
+app.controller( 'Main', ['$scope', '$http', function($scope, $http) {
   $http.get('wp-json/posts/').success(function(res){
-    console.log(res);
     $scope.posts = res;
   });
-})
-.controller('Content', function( $scope, $http, $routeParams ){
-  $http.get('wp-json/posts/' + $routeParams.ID).success(function(res){
-    $scope.post = res;
+}]);
+
+// Content Controller
+app.controller('Content', ['$scope', '$http', '$routeParams', function( $scope, $http, $routeParams ){
+  $http.get('wp-json/posts/?filter[name]=' + $routeParams.slug).success(function(res){
+    $scope.post = res[0];
   });
+}]);
+
+// Search Form Directive
+app.directive('searchForm', function(){
+  return {
+    restrict: 'EA',
+    templateUrl: bpLocalized.partials + 'search-form.html',
+    controller: function ( $scope, $http ) {
+      $scope.filter = {
+        s: ''
+      }
+      $scope.search = function() {
+        $http.get( 'wp-json/posts/?filter[s]=' + $scope.filter.s ).success(function(res){
+          $scope.posts = res;
+        });
+      }
+    }
+  };
 });
