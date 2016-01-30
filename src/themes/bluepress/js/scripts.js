fea49fea7b8dba4ba3329de260db4a59b5447898
Luis Fernando Mendoza <goliat8903@gmail.com>
2016-01-29 19:44:33 -0600

    Adds category route, controller and template

--- src/themes/bluepress/js/scripts.js
+++ src/themes/bluepress/js/scripts.js
@@ -20,6 +20,11 @@ app.config(['$routeProvider', '$locationProvider', function( $routeProvider, $lo
     controller: 'Content'
   })
 
+  .when('/category/:category', {
+    templateUrl: bpLocalized.partials + 'main.html',
+    controller: 'Category'
+  })
+
   .otherwise({
     redirectTo: '/'
   });
@@ -28,8 +33,16 @@ app.config(['$routeProvider', '$locationProvider', function( $routeProvider, $lo
 
 // Main Controller
 app.controller( 'Main', ['$scope', '$http', function($scope, $http) {
+
+  $http.get('wp-json/taxonomies/category/terms').success(function(res){
+    $scope.categories = res;
+  });
+
   $http.get('wp-json/posts/').success(function(res){
     $scope.posts = res;
+    $scope.pageTitle = 'Latest Posts:';
+    //console.log(JSON.stringify(res));
+    document.querySelector('title').innerHTML = 'Home | Angular + Wordpress';
   });
 }]);
 
@@ -37,7 +50,28 @@ app.controller( 'Main', ['$scope', '$http', function($scope, $http) {
 app.controller('Content', ['$scope', '$http', '$routeParams', function( $scope, $http, $routeParams ){
   $http.get('wp-json/posts/?filter[name]=' + $routeParams.slug).success(function(res){
     $scope.post = res[0];
+    document.querySelector('title').innerHTML = res[0].title + ' Angular + Wordpress';
   });
+}]);
+
+// Category Controller
+app.controller('Category', ['$scope', '$http', '$routeParams', function( $scope, $http, $routeParams ){
+  $http.get('wp-json/taxonomies/category/terms').success(function(res){
+    $scope.categories = res;
+    console.log(JSON.stringify(res));
+  });
+
+  $http.get('wp-json/taxonomies/category/terms' + $routeParams.category).success(function(res){
+    $scope.current_category_id = $routeParams.category;
+    $scope.pageTitle = 'Posts in ' + res.name + ':';
+    document.querySelector('title').innerHTML = 'Category ' +  res.name + ' | AngularJs Demo Theme';
+
+    $http.get('wp-json/posts/?filter[category_name]=' + res.name).success(function(res){
+      $scope.posts = res;
+    });
+  });
+
+
 }]);
 
 // Search Form Directive
